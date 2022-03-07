2c8667c7952bdf4b6c53526e43e1f3dfbc450d29
416Shemi <tu7ngcsvr@code.edu.az>
2022-03-07 23:53:58 +0400

    new projekt3

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arry = { 14, 4, 16, 78, 53 };
+            Console.WriteLine(Min(arry));
+            Console.ReadLine();
+            
+        }
+        static int Min(int[] arry)
+        {
+            int min = arry[0];
+            for (int i = 0; i <arry.Length; i++)
+            {
+                if (arry[i] < arry[0])
+                {
+                    min = arry[i];
+                }
+            }
+            return min;
+        }
+        
+        
+    }
+}
